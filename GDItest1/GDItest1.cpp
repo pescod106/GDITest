d4488ef0bc3e8568517148d5b29d0baf439d294a
Pescod <pescod106@gmail.com>
2016-04-04 15:21:28 +0800

    按照作业顺序已经完成第六道

--- GDItest1/GDItest1.cpp
+++ GDItest1/GDItest1.cpp
@@ -6,6 +6,8 @@
 #include <math.h>
 #define MAX_LOADSTRING 100
 #define MAX_COUNT 10
+#define LINECOUNT 100
+#define GET_Y_LPARAM(lp) ((int)(short)HIWORD(lp))
 // 全局变量: 
 HINSTANCE hInst;								// 当前实例
 TCHAR szTitle[MAX_LOADSTRING];					// 标题栏文本
@@ -16,6 +18,18 @@ ATOM				MyRegisterClass(HINSTANCE hInstance);
 BOOL				InitInstance(HINSTANCE, int);
 LRESULT CALLBACK	WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK	About(HWND, UINT, WPARAM, LPARAM);
+VOID LineMouseDown(DRAWSTRU pDs, HWND hWnd, WPARAM wParam, LPARAM lParam);
+VOID LineMouseUp(DRAWSTRU pDs, HWND hWnd, WPARAM wParam, LPARAM lParam);
+VOID LineMouseMove(DRAWSTRU pDs, HWND hWnd, WPARAM wParam, LPARAM lParam);
+
+VOID RectMouseDown(PDRAWSTRU pDs, HWND hWnd, WPARAM wParam, LPARAM lParam);
+VOID RectMouseUp(PDRAWSTRU pDs, HWND hWnd, WPARAM wParam, LPARAM lParam);
+VOID RectMouseMove(PDRAWSTRU pDs, HWND hWnd, WPARAM wParam, LPARAM lParam);
+
+VOID EllipseMouseDown(PDRAWSTRU pDs, HWND hWnd, WPARAM wParam, LPARAM lParam);
+VOID EllipseMouseUp(PDRAWSTRU pDs, HWND hWnd, WPARAM wParam, LPARAM lParam);
+VOID EllipseMouseMove(PDRAWSTRU pDs, HWND hWnd, WPARAM wParam, LPARAM lParam);
+
 
 int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
                      _In_opt_ HINSTANCE hPrevInstance,
@@ -123,23 +137,253 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 //  WM_DESTROY	- 发送退出消息并返回
 //
 //
+
+VOID LineMouseDown(PDRAWSTRU pDs, HWND hWnd, WPARAM wParam, LPARAM lParam)
+{
+	SetCapture(hWnd);
+	pDs->bMouseDown = TRUE;
+	pDs->ptStart.x = LOWORD(lParam);
+	pDs->ptStart.y = GET_Y_LPARAM(lParam);
+	pDs->ptEnd.x = pDs->ptStart.x;
+	pDs->ptEnd.y = pDs->ptStart.y;
+}
+
+VOID LineMouseUp(PDRAWSTRU pDs, HWND hWnd, WPARAM wParam, LPARAM lParam)
+{
+	ReleaseCapture();
+	pDs->bMouseDown = false;
+	HDC hdc = GetDC(hWnd);
+
+	SetROP2(hdc, R2_NOT);
+	MoveToEx(hdc, pDs->ptStart.x, pDs->ptStart.y, NULL);
+	LineTo(hdc, pDs->ptEnd.x, pDs->ptEnd.y);
+
+	pDs->ptEnd.x = LOWORD(lParam);
+	pDs->ptEnd.y = GET_Y_LPARAM(lParam);
+	SetROP2(hdc, R2_COPYPEN);
+	HPEN hPenOld = (HPEN)SelectObject(hdc, pDs->hPen);
+	MoveToEx(hdc, pDs->ptStart.x, pDs->ptStart.y, NULL);
+	LineTo(hdc, pDs->ptEnd.x, pDs->ptEnd.y);
+	SelectObject(hdc, hPenOld);
+	ReleaseDC(hWnd, hdc);
+
+	/*if (iLineIndex<LINECOUNT - 1)
+	{
+		iLineIndex++;
+		arrLine[iLineIndex].ptStart.x = ptStart.x;
+		arrLine[iLineIndex].ptStart.y = ptStart.y;
+		arrLine[iLineIndex].ptEnd.x = ptEnd.x;
+		arrLine[iLineIndex].ptEnd.y = ptEnd.y;
+	}*/
+}
+
+VOID LineMouseMove(PDRAWSTRU pDs, HWND hWnd, WPARAM wParam, LPARAM lParam)
+{
+	HDC hdc;
+	if (pDs->bMouseDown)
+	{
+		hdc = GetDC(hWnd);
+
+		SetROP2(hdc, R2_NOT);
+		MoveToEx(hdc, pDs->ptStart.x, pDs->ptStart.y, NULL);
+		LineTo(hdc, pDs->ptEnd.x, pDs->ptEnd.y);
+
+		pDs->ptEnd.x = LOWORD(lParam);
+		pDs->ptEnd.y = GET_Y_LPARAM(lParam);
+		MoveToEx(hdc, pDs->ptStart.x, pDs->ptStart.y, NULL);
+		LineTo(hdc, pDs->ptEnd.x, pDs->ptEnd.y);
+		ReleaseDC(hWnd, hdc);
+	}
+}
+
+VOID RectMouseDown(PDRAWSTRU pDs, HWND hWnd, WPARAM wParam, LPARAM lParam)
+{
+	SetCapture(hWnd);
+	pDs->bMouseDown = TRUE;
+	pDs->ptStart.x = LOWORD(lParam);
+	pDs->ptStart.y = GET_Y_LPARAM(lParam);
+	pDs->ptEnd.x = pDs->ptStart.x;
+	pDs->ptEnd.y = pDs->ptStart.y;
+}
+
+VOID RectMouseUp(PDRAWSTRU pDs, HWND hWnd, WPARAM wParam, LPARAM lParam)
+{
+	ReleaseCapture();
+	pDs->bMouseDown = false;
+	HDC hdc = GetDC(hWnd);
+
+	SetROP2(hdc, R2_NOT);
+	Rectangle(hdc, pDs->ptStart.x, pDs->ptStart.y, pDs->ptEnd.x, pDs->ptEnd.y);
+
+	pDs->ptEnd.x = LOWORD(lParam);
+	pDs->ptEnd.y = GET_Y_LPARAM(lParam);
+	SetROP2(hdc, R2_COPYPEN);
+	HPEN hPenOld = (HPEN)SelectObject(hdc, pDs->hPen);
+	Rectangle(hdc, pDs->ptStart.x, pDs->ptStart.y, pDs->ptEnd.x, pDs->ptEnd.y);
+	SelectObject(hdc, hPenOld);
+	ReleaseDC(hWnd, hdc);
+}
+
+VOID RectMouseMove(PDRAWSTRU pDs, HWND hWnd, WPARAM wParam, LPARAM lParam)
+{
+	HDC hdc;
+	if (pDs->bMouseDown)
+	{
+		hdc = GetDC(hWnd);
+
+		SetROP2(hdc, R2_NOT);
+		Rectangle(hdc, pDs->ptStart.x, pDs->ptStart.y, pDs->ptEnd.x, pDs->ptEnd.y);
+
+		pDs->ptEnd.x = LOWORD(lParam);
+		pDs->ptEnd.y = GET_Y_LPARAM(lParam);
+		Rectangle(hdc, pDs->ptStart.x, pDs->ptStart.y, pDs->ptEnd.x, pDs->ptEnd.y);
+		ReleaseDC(hWnd, hdc);
+	}
+}
+
+VOID EllipseMouseDown(PDRAWSTRU pDs, HWND hWnd, WPARAM wParam, LPARAM lParam)
+{
+	SetCapture(hWnd);
+	pDs->bMouseDown = TRUE;
+	pDs->ptStart.x = LOWORD(lParam);
+	pDs->ptStart.y = GET_Y_LPARAM(lParam);
+	pDs->ptEnd.x = pDs->ptStart.x;
+	pDs->ptEnd.y = pDs->ptStart.y;
+}
+
+VOID EllipseMouseUp(PDRAWSTRU pDs, HWND hWnd, WPARAM wParam, LPARAM lParam)
+{
+	ReleaseCapture();
+	pDs->bMouseDown = false;
+	HDC hdc = GetDC(hWnd);
+
+	SetROP2(hdc, R2_NOT);
+	Ellipse(hdc, pDs->ptStart.x, pDs->ptStart.y, pDs->ptEnd.x, pDs->ptEnd.y);
+
+	pDs->ptEnd.x = LOWORD(lParam);
+	pDs->ptEnd.y = GET_Y_LPARAM(lParam);
+	SetROP2(hdc, R2_COPYPEN);
+	HPEN hPenOld = (HPEN)SelectObject(hdc, pDs->hPen);
+	Ellipse(hdc, pDs->ptStart.x, pDs->ptStart.y, pDs->ptEnd.x, pDs->ptEnd.y);
+	SelectObject(hdc, hPenOld);
+	ReleaseDC(hWnd, hdc);
+}
+
+VOID EllipseMouseMove(PDRAWSTRU pDs, HWND hWnd, WPARAM wParam, LPARAM lParam)
+{
+	HDC hdc;
+	if (pDs->bMouseDown)
+	{
+		hdc = GetDC(hWnd);
+
+		SetROP2(hdc, R2_NOT);
+		Ellipse(hdc, pDs->ptStart.x, pDs->ptStart.y, pDs->ptEnd.x, pDs->ptEnd.y);
+
+		pDs->ptEnd.x = LOWORD(lParam);
+		pDs->ptEnd.y = GET_Y_LPARAM(lParam);
+		Ellipse(hdc, pDs->ptStart.x, pDs->ptStart.y, pDs->ptEnd.x, pDs->ptEnd.y);
+		ReleaseDC(hWnd, hdc);
+	}
+}
+
 int s[100];
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
+	static PDRAWFUN pMouseDown, pMouseUp, pMouseMove;
 	int wmId, wmEvent;
 	PAINTSTRUCT ps;
 	HDC hdc;
 	int i;
 	RECT rt;
+	static POINT ptStart, ptEnd;
 	static UINT nHeight;
+	static bool bMouseDown = false;
+
+	static LINE arrLine[LINECOUNT];
+	static int iLineIndex = 1;
+
+	static DRAWSTRU ds;
+
+	HMENU hMenu = GetMenu(hWnd);
 	switch (message)
 	{
+	case WM_CREATE:
+		pMouseDown = LineMouseDown;
+		pMouseUp = LineMouseUp;
+		pMouseMove = LineMouseMove;
+		ds.hPen = CreatePen(PS_SOLID, 1, RGB(255, 0, 0));
+		break;
+	case WM_LBUTTONDOWN:
+		pMouseDown(&ds, hWnd, wParam, lParam);
+		break;
+	case WM_LBUTTONUP:
+		pMouseUp(&ds, hWnd, wParam, lParam);
+		break;
+	case WM_MOUSEMOVE:
+		pMouseMove(&ds, hWnd, wParam, lParam);
+		break;
 	case WM_COMMAND:
 		wmId    = LOWORD(wParam);
 		wmEvent = HIWORD(wParam);
+
 		// 分析菜单选择: 
 		switch (wmId)
 		{
+		case IDM_COLOR_RED:
+			DeleteObject(ds.hPen);
+			ds.hPen = CreatePen(PS_SOLID, 1, RGB(255, 0, 0));
+
+			break;
+		case IDM_COLOR_GREEN:
+			DeleteObject(ds.hPen);
+			ds.hPen = CreatePen(PS_SOLID, 1, RGB(0, 255, 0));
+			break;
+		case IDM_COLOR_BLUE:
+			DeleteObject(ds.hPen);
+			ds.hPen = CreatePen(PS_SOLID, 1, RGB(0, 0, 255));
+			break;
+		case IDM_DRAW_LINE:
+			pMouseDown = LineMouseDown;
+			pMouseUp = LineMouseUp;
+			pMouseMove = LineMouseMove;
+			CheckMenuItem(hMenu, IDM_DRAW_LINE, MF_BYCOMMAND | MF_CHECKED);
+			CheckMenuItem(hMenu, IDM_DRAW_RECT, MF_BYCOMMAND | MF_UNCHECKED);
+			CheckMenuItem(hMenu, IDM_DRAW_ELLIPSE, MF_BYCOMMAND | MF_UNCHECKED);
+			break;
+		case IDM_DRAW_RECT:
+			pMouseDown = RectMouseDown;
+			pMouseUp = RectMouseUp;
+			pMouseMove = RectMouseMove;
+			CheckMenuItem(hMenu, IDM_DRAW_LINE, MF_BYCOMMAND | MF_UNCHECKED);
+			CheckMenuItem(hMenu, IDM_DRAW_RECT, MF_BYCOMMAND | MF_CHECKED);
+			CheckMenuItem(hMenu, IDM_DRAW_ELLIPSE, MF_BYCOMMAND | MF_UNCHECKED);
+			break;
+		case IDM_DRAW_ELLIPSE:
+			pMouseDown = EllipseMouseDown;
+			pMouseUp = EllipseMouseUp;
+			pMouseMove = EllipseMouseMove;
+			CheckMenuItem(hMenu, IDM_DRAW_LINE, MF_BYCOMMAND | MF_UNCHECKED);
+			CheckMenuItem(hMenu, IDM_DRAW_RECT, MF_BYCOMMAND | MF_UNCHECKED);
+			CheckMenuItem(hMenu, IDM_DRAW_ELLIPSE, MF_BYCOMMAND | MF_CHECKED);
+			break;
+		case IDM_DRAW_LINE1:
+			hdc = GetDC(hWnd);
+			MoveToEx(hdc, 100, 100, NULL);
+			LineTo(hdc, 600, 100);
+			ReleaseDC(hWnd, hdc); 
+			break;
+		case IDM_DRAW_TITLE:
+			hdc = GetWindowDC(hWnd);
+			MoveToEx(hdc, 100, 100, NULL);
+			LineTo(hdc, 600, 100);
+			ReleaseDC(hWnd, hdc);
+			break;
+		case IDM_DRAW_DESKTOP:
+			hdc = GetWindowDC(NULL);
+			MoveToEx(hdc, 100, 100, NULL);
+			LineTo(hdc, 600, 100);
+			ReleaseDC(hWnd, hdc);
+			break;
 		case IDM_ABOUT:
 			DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
 			break;
@@ -153,17 +397,52 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 	case WM_PAINT:
 		hdc = BeginPaint(hWnd, &ps);
 		// TODO:  在此添加任意绘图代码...
-		GetClientRect(hWnd, &rt);
-		/*for (i = 0; i < 100; i++)
-		{
-			s[i] = rt.bottom / 2 * (1 - sin(i * 2 * 3.14 / 100));
-		}*/
+		/*GetClientRect(hWnd, &rt);
 		
 		MoveToEx(hdc,0, rt.bottom / 2, NULL);
 		for (i = 0; i < MAX_COUNT; i++)
 		{
 			LineTo(hdc, rt.right*i / MAX_COUNT, s[i]);
 		}
+		MoveToEx(hdc, 100, 200, NULL);
+		LineTo(hdc,600,200);*/
+
+
+
+		/*HPEN hPen, hPenOld;
+		HBRUSH hBr, hBrOld;
+
+		hPen = CreatePen(PS_SOLID, 2, RGB(255, 0, 0));//生成一个红色画笔
+		hPenOld = (HPEN)SelectObject(hdc, hPen);//选入画笔
+		hBr = CreateSolidBrush(RGB(0, 0, 255));//生成蓝色画刷
+		hBrOld = (HBRUSH)SelectObject(hdc, hBr);
+
+		//从(100,50)到(600,50)画一条线段
+		MoveToEx(hdc, 100, 50, NULL);
+		LineTo(hdc, 600, 50);
+
+		Rectangle(hdc, 100, 100, 300, 200);//画一个红色边线，蓝色填充的矩形
+		Ellipse(hdc, 400, 100, 600, 200);//画一个红色边线，蓝色填充的椭圆
+
+		SelectObject(hdc, GetStockObject(NULL_PEN));;//选入一个空的画笔
+		Rectangle(hdc, 100, 230, 300, 330);//画一个没有边框，蓝色的矩形
+		Ellipse(hdc, 400, 230, 600, 330);//画一个没有边框，蓝色的椭圆
+
+		SelectObject(hdc, hPen);
+		SelectObject(hdc, GetStockObject(NULL_BRUSH));
+		Rectangle(hdc, 100, 400, 300, 500);
+		Ellipse(hdc, 400, 400, 600, 500);
+
+		SelectObject(hdc, hPenOld);
+		DeleteObject(hPen);
+		DeleteObject(hBr);*/
+		for (int i = 0; i <= iLineIndex; i++)
+		{
+			MoveToEx(hdc, arrLine[i].ptStart.x, arrLine[i].ptStart.y, NULL);
+			LineTo(hdc, arrLine[i].ptEnd.x, arrLine[i].ptEnd.y);
+		}
+		
+
 		EndPaint(hWnd, &ps);
 		break;
 	case WM_SIZE:
--- GDItest1/GDItest1.h
+++ GDItest1/GDItest1.h
@@ -1,3 +1,19 @@
 #pragma once
 
 #include "resource.h"
+
+typedef struct tagLine
+{
+	POINT ptStart;
+	POINT ptEnd;
+}LINE;
+
+typedef struct tagDrawStru
+{
+	POINT ptStart;
+	POINT ptEnd;
+	BOOL bMouseDown;
+	HPEN hPen;
+} DRAWSTRU,*PDRAWSTRU;
+
+typedef VOID(*PDRAWFUN)(PDRAWSTRU pDs, HWND hWnd, WPARAM wParam, LPARAM lParam);
